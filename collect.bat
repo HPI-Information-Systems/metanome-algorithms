d301f1043463da44aaef7b32c95d8f4278c91e92
Thorsten Papenbrock <thorsten.papenbrock@hpi.de>
2014-10-28 11:39:53 +0100

    Added collect.bat to collect the relevant algorithm jars for Metanome from a build of this repository

---
+++ collect.bat
@@ -0,0 +1,26 @@
+if not exist _COLLECTION_ (
+  mkdir _COLLECTION_
+)
+
+copy SPIDER\SPIDERFile\target\*.jar _COLLECTION_
+copy SPIDER\SPIDERDatabase\target\*.jar _COLLECTION_
+
+copy BINDER\BINDERFile\target\*.jar _COLLECTION_
+copy BINDER\BINDERDatabase\target\*.jar _COLLECTION_
+
+copy tane\tane_algorithm\target\*.jar _COLLECTION_
+::copy tane\tane_tree_dir_algorithm\target\*.jar _COLLECTION_
+::copy tane\tane_tree_end_algorithm\target\*.jar _COLLECTION_
+
+copy fun\fun_for_metanome\target\*.jar _COLLECTION_
+
+copy fdmine\fdmine_algorithm\target\*.jar _COLLECTION_
+
+copy depminer\depminer_algorithm\target\*.jar _COLLECTION_
+
+copy fastfds\fastfds_algorithm\target\*.jar _COLLECTION_
+
+copy fdep\fdep_algorithm\target\*.jar _COLLECTION_
+
+copy ducc\ducc_for_metanome\target\*.jar _COLLECTION_
+
