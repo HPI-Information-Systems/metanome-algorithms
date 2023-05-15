8e4aa9da2e11218a13b1ce3a3a0e632d263c8037
Torben Eims <torben.eims@stud.uni-hannover.de>
2023-05-15 16:25:53 +0200

    Refactoring ColumnCollection: Merged superfluous setting of numberOfColumns from master

--- dfd/dfdAlgorithm/src/fdiscovery/columns/DifferenceSet.java
+++ dfd/dfdAlgorithm/src/fdiscovery/columns/DifferenceSet.java
@@ -4,12 +4,9 @@ package fdiscovery.columns;
 public class DifferenceSet extends ColumnCollection {
 
 	private static final long serialVersionUID = -5174627424398542681L;
-	
-	private long numberOfColumns;
-	
+
 	public DifferenceSet(AgreeSet agreeSet) {
 		super(agreeSet.getNumberOfColumns());
-		this.numberOfColumns = agreeSet.getNumberOfColumns();
 		
 		this.or(agreeSet);
 		this.flip(0, this.numberOfColumns);
